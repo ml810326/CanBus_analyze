bc87a263321f6053e1ff101fe39092437319f915
tomchang <tomchang@via.com.tw>
2020-06-19 18:03:25 +0800

    update speed and gear analyze

--- ana.c
+++ ana.c
@@ -2,12 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-void check_ID(char *id){
- if(strcmp(id, "18FEF100")==0){
-    printf("speed data\n");
+// hex to number
+float getNum(char ch){
+  float num = 0;
+  if(ch>='0' && ch<='9'){
+    num = ch - '0';
+  }else{
+    switch(ch)
+    {
+      case 'A': case 'a': num = 10; break;
+      case 'B': case 'b': num = 11; break;
+      case 'C': case 'c': num = 12; break;
+      case 'D': case 'd': num = 13; break;
+      case 'E': case 'e': num = 14; break;
+      case 'F': case 'f': num = 15; break;
+      default: num=0;
+    }
   }
+  return num;
 }
 
+float HextoInt(char* hex1, char* hex2){
+  float speed = (4096*getNum(hex2[0]) + 256*getNum(hex2[1]) + 16*getNum(hex1[0]) + getNum(hex1[1]))/256;
+  return speed;
+}
+
+int check_gear(char* hex1){
+  if(hex1[0]=='4'&&hex1[1]=='E'){
+    return 1;
+  }else if(hex1[0]=='5'&&hex1[1]=='2'){
+    return 2;
+  }else if(hex1[0]=='4'&&hex1[1]=='4'){
+    return 3;
+  }else{
+    return 4;
+  }
+  //printf("char1: %c char2: %c\n",hex1[0],hex1[1]); 
+}
+
+// print times data
+void check_times(char *times){
+  printf("time:%s\n", times);
+}
+
+//Check the data ID
+int check_ID(char *id){
+  if(strcmp(id, "18FEF100")==0){
+    printf("data is speed\n");
+    return 1;
+  }
+  if(strcmp(id, "18F00503")==0){
+    printf("data is gear\n");
+    return 2;
+  }
+  return 0;
+}
+
+// Check extend or base frame
 void check_ext(char *ex){
   if(strcmp(ex, "1")==0){
     printf("Extend Frame\n");
@@ -16,6 +67,7 @@ void check_ext(char *ex){
   }
 }
 
+//Check frame
 void check_remo(char *rem){
   if(strcmp(rem, "1")==0){
     printf("Remote Frame\n");
@@ -24,56 +76,88 @@ void check_remo(char *rem){
   }
 }
 
+// print the meaning of data frame
+void check_mean(int cata, char **datas){
+  if(cata==1){
+    float speed = HextoInt(datas[1], datas[2]);
+    printf("speed data: %f\n", speed);
+  }
+  if(cata==2){
+    printf("Gear data is %s\n", datas[7]);
+    int gear_data = check_gear(datas[7]);
+    if(gear_data==1){
+      printf("Gear: N\n");
+    }else if(gear_data==2){
+      printf("Gear: R\n");
+    }else if(gear_data==3){
+      printf("Gear: D\n");
+    }else{
+      printf("Not define data\n");
+    }
+    /*
+    if(strcmp(datas[7], "4E")==0){
+      printf("Gear data: N\n");
+    }else if(strcmp(datas[7], "52")==0){
+      printf("Gear data: R\n");
+    }else if(strcmp(datas[7], "44")==0){
+      printf("Gear data: D\n");
+    }else{
+      printf("Gear data is not define\n");
+    }
+    */
+  }
+}
+
+//main function
 int main(){
   FILE *fp;
   char ch;
+  char *line_buf = NULL;
+  size_t line_buf_size = 0;
+  ssize_t line_size;
   int i=0, count=0;
+  char * delims = ",";
 
-  if((fp=fopen("test.can","r"))==NULL){
+  if((fp=fopen("test.can","rt"))==NULL){
     printf("open file error!\n");
     return -1;
   }
+  
+  line_size = getline(&line_buf, &line_buf_size, fp);
+  
+  /*
+  while(line_size >= 0){
+    printf("content: %s\n", line_buf);
+    char *test1 = strtok(line_buf, delims);
+    printf("contenet1: %s\n", test1);
+    char *test2 = strtok(NULL, delims);
+    printf("contenet2: %s\n", test2);
+    line_size = getline(&line_buf, &line_buf_size, fp);
+  }
+  */
 
-  char times[20];
-  fscanf(fp, "%[^,]%*c",times);
-  printf("%s\n", times);
+  while(line_size >= 0){
+    char *times = strtok(line_buf, delims);
+    check_times(times);
 
-  char ext[20];
-  fscanf(fp, "%[^,]%*c",ext);
-  check_ext(ext);  
+    char *ext = strtok(NULL, delims);
+    check_ext(ext);  
 
-  char remo[20];
-  fscanf(fp, "%[^,]%*c",remo);
-  check_remo(remo);
+    char *remo = strtok(NULL, delims);
+    check_remo(remo);
 
-  char ids[20];  
-  fscanf(fp, "%[^,]%*c",ids);
-  check_ID(ids);
+    char *ids = strtok(NULL, delims);
+    int cata = check_ID(ids);
 
-  char datas[8][20];
-  fscanf(fp, "%[^,]%*c",datas[0]);
-  printf("%s\n", datas[0]);
+    char *datas[8];
+    for(int i=0;i<8;++i){
+        datas[i] = strtok(NULL, delims);
+    }
 
-  fscanf(fp, "%[^,]%*c",datas[1]);
-  printf("%s\n", datas[1]);
+    check_mean(cata, datas);
 
-  fscanf(fp, "%[^,]%*c",datas[2]);
-  printf("%s\n", datas[2]);
-
-  fscanf(fp, "%[^,]%*c",datas[3]);
-  printf("%s\n", datas[3]);
-
-  fscanf(fp, "%[^,]%*c",datas[4]);
-  printf("%s\n", datas[4]);
-
-  fscanf(fp, "%[^,]%*c",datas[5]);
-  printf("%s\n", datas[5]);
-
-  fscanf(fp, "%[^,]%*c",datas[6]);
-  printf("%s\n", datas[6]);
-
-  fscanf(fp, "%[^,]%*c",datas[7]);
-  printf("%s\n", datas[7]);
+    line_size = getline(&line_buf, &line_buf_size, fp);
+  }
 
   fclose(fp);
   return 0;
